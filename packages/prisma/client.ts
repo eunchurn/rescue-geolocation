79522ed53b08b05a6170bf429061e7b03b315d80
Eunchurn Park <eunchurn.park@gmail.com>
2024-04-01 01:44:17 +0900

    fix(db): to mongodb

--- packages/prisma/client.ts
+++ packages/prisma/client.ts
@@ -1,31 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import ws from "ws";
 
-// export const getPrisma = () => {
-//   console.log("== generating prisma instance ==");
-//   if (process.env.NODE_ENV === "production") {
-//     neonConfig.webSocketConstructor = ws;
-//     const connectionString = `${process.env.DATABASE_URL}`;
-
-//     const pool = new Pool({ connectionString });
-//     const adapter = new PrismaNeon(pool);
-//     const prisma = new PrismaClient({ adapter });
-//     return prisma;
-//   } else {
-//     const prisma = new PrismaClient();
-//     return prisma;
-//   }
-// };
-
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-export const prisma = new PrismaClient({ adapter })
-// export const prisma =
-//   process.env.NODE_ENV === "production"
-//     ? new PrismaClient({ adapter })
-//     : new PrismaClient();
+export const prisma = new PrismaClient()
