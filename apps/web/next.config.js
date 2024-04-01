765dac54b3db12f216ef37327f2fb65b8530ccec
Eunchurn Park <eunchurn.park@gmail.com>
2024-04-02 01:24:47 +0900

    chore(meta): check

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@rescue/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vercel.app",
+      },
+    ],
+  },
   async headers() {
     return [
       {
@@ -8,37 +16,37 @@ module.exports = {
         headers: [
           {
             key: "Cache-Control",
-            value: "no-store, max-age=0"
-          }
-        ]
+            value: "no-store, max-age=0",
+          },
+        ],
       },
       {
         source: "/create",
         headers: [
           {
             key: "Cache-Control",
-            value: "no-store, max-age=0"
-          }
-        ]
+            value: "no-store, max-age=0",
+          },
+        ],
       },
       {
         source: "/rescue",
         headers: [
           {
             key: "Cache-Control",
-            value: "no-store, max-age=0"
-          }
-        ]
+            value: "no-store, max-age=0",
+          },
+        ],
       },
       {
         source: "/tracking",
         headers: [
           {
             key: "Cache-Control",
-            value: "no-store, max-age=0"
-          }
-        ]
-      }
-    ]
-  }
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
 };
