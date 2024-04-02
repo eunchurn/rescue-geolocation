f46fd1c86e7829d2a442fb7108b47a99f3a577fe
Eunchurn Park <eunchurn.park@gmail.com>
2024-04-02 16:19:18 +0900

    chore(loader): in button

--- apps/web/components/rescueButton/index.tsx
+++ apps/web/components/rescueButton/index.tsx
@@ -7,13 +7,20 @@ import { useRouter } from "next/navigation";
 
 export function RescueButton() {
   const router = useRouter();
+  const [active, setActive] = React.useState(true);
   const handleCreate = React.useCallback(() => {
+    setActive(false);
     router.push(`/create/${nanoid(6)}`);
   }, [router]);
   return (
     <button
-      className={[styles.button, styles.buttonHelp].join(" ")}
+      className={[
+        styles.button,
+        styles.buttonHelp,
+        active ? "" : styles.loading,
+      ].join(" ")}
       onClick={handleCreate}
+      disabled={!active}
     >
       구조링크 생성
     </button>
