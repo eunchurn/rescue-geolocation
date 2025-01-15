1f3065cd8bbda01a1eb8f1377037f3c66774e485
Eunchurn Park <eunchurn.park@gmail.com>
2025-01-15 15:27:12 +0900

    chore(prisma): castaway updated

--- apps/web/app/api/cron/route.ts
+++ apps/web/app/api/cron/route.ts
@@ -6,7 +6,11 @@ export async function GET() {
   const data = await prisma.geoLocation.deleteMany({
     where: { createdAt: { lte: dayjs().subtract(1, "day").toDate() } },
   });
-  console.log(`== deleted ${JSON.stringify(data)}`)
+  const castaway = await prisma.castaway.deleteMany({
+    where: { createdAt: { lte: dayjs().subtract(1, "day").toDate() } },
+  });
+  console.log(`== deleted location ${JSON.stringify(data)}`);
+  console.log(`== deleted castaway ${JSON.stringify(castaway)}`);
   return Response.json(data);
 }
 
